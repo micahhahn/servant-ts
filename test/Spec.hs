299438300ee3aae8992f1b218cb80d9ba44cc497
Micah Hahn <micah.s.hahn@gmail.com>
2019-03-19 01:17:35 -0400

    Improve error messages

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,6 +6,7 @@
 import Data.Aeson
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Text (Text)
 import Data.Typeable
 import GHC.Generics
 import Test.Tasty
@@ -14,23 +15,31 @@ import Test.Tasty.HUnit
 import Servant.TS
 
 {- Determines if a given JSON value can be represented as a TsType -}
-isValid :: TsContext TsType -> Value -> Bool
-isValid (TsContext TsBoolean _) (Bool _) = True
-isValid (TsContext (TsRef t) m) v = case Map.lookup t m of
+isValid :: TsContext TsType -> Value -> Either () (TsContext TsType, Value)
+isValid (TsContext TsBoolean _) (Bool _) = Left ()
+isValid (TsContext TsNumber _) (Number _) = Left ()
+isValid tt@(TsContext (TsRef t) m) v = case Map.lookup t m of
     Just t' -> isValid (TsContext t' m) v
-    Nothing -> False
-isValid _ _ = False
+    Nothing -> Right (tt, v)
+isValid t v = Right (t, v)
 
 main = defaultMain tests
 
-data X = X Int
+data X = X Int Bool Text
     deriving (Generic, Typeable, ToJSON, TsTypeable)
 
 makeTest :: forall a. (ToJSON a, TsTypeable a) => a -> Assertion
 makeTest v = let t = tsTypeRep (Proxy :: Proxy a)
                  j = toJSON v
-              in isValid t j @? "Failed to unify JSON value with TS type:\n    Value:  " ++ show j ++ "\n    TsType: " ++ show t
+              in case isValid t j of
+                     Left _ -> True @? ""
+                     Right (TsContext t' _, j') -> False @? "Failed to unify JSON value with TS type:\n" 
+                                             ++ "    Value:  " ++ show j' ++ "\n" 
+                                             ++ "    TsType: " ++ show t' ++ "\n" 
+                                             ++ "Initial types:\n" 
+                                             ++ "    Value:  " ++ show j ++ "\n" 
+                                             ++ "    TsType: " ++ show t
 
 tests :: TestTree
 tests = testGroup "Aeson <-> TS deriving isomorphic" 
-    [ testCase "Basic unnamed type" $ makeTest (X 0)]
+    [ testCase "Basic unnamed type" $ makeTest (X 0 False "a")]
