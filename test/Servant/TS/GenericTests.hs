bc43d47193af403bb8103322ae0f3bb749f24deb
Micah Hahn <micah.s.hahn@gmail.com>
2019-04-02 00:40:07 -0400

    Add record tests

--- test/Servant/TS/GenericTests.hs
+++ test/Servant/TS/GenericTests.hs
@@ -24,10 +24,25 @@ data Union = UnionLeft Int
            | UnionRight Text
     deriving (Show, Generic, Typeable, ToJSON, TsTypeable)
 
+data RecordUnion = RUnionLeft { a :: Int }
+                 | RUnionRight { b :: Text, c :: Bool }
+                 deriving (Show, Generic, Typeable, ToJSON, TsTypeable)
+
+data BadUnion = BUnionLeft { tag :: Int, d :: Text }
+              | BUnionRight Text
+              deriving (Show, Generic, Typeable, ToJSON, TsTypeable)
+
 genericTests :: TestTree
 genericTests = testGroup "Aeson <-> TS generic deriving isomorphic"
     [ makeTest (X 0 False "a")
+
     , makeTest (UnionLeft 7)
     , makeTest UnionMiddle
     , makeTest (UnionRight "a")
+
+    , makeTest (RUnionLeft 7)
+    , makeTest (RUnionRight "a" False)
+
+    , makeTest (BUnionLeft 7 "a")
+    , makeTest (BUnionRight "b")
     ]
