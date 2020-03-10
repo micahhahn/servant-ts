45ce574a74c55e71fecf5241cb985145b0d0868a
Micah Hahn <micah.s.hahn@gmail.com>
2020-03-10 00:22:09 -0400

    Juice up testing

--- test/Servant/TS/GenericTests.hs
+++ test/Servant/TS/GenericTests.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE EmptyDataDeriving #-}
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Servant.TS.GenericTests (
     genericTests
@@ -31,69 +36,162 @@ import Servant.TS.TestHelpers
 import Language.Haskell.TH
 import Language.Haskell.TH.Ppr
 
+import qualified Data.HashMap.Strict as HM 
+
+data X = X Int Bool
+
+instance TsTypeable X where
+    tsTypeRep _ = TsTuple [ts @Int, ts @Bool, ts @X]
+
+ts :: forall a. TsTypeable a => TsType
+ts = tsTypeRep (Proxy :: Proxy a)
+
+-- Test basic record field mapping
 data TFieldTest = CFieldTest1 { fFieldTest1A :: Int, fFieldTest1B :: Int }
-                | CFieldTest2 { fFieldTest2A :: Bool, fFIeldTest2B :: Text }
-                deriving (Show, Typeable)
+                | CFieldTest2 { fFieldTest2A :: Bool, fFieldTest2B :: Text }
+                deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions ''TFieldTest
 
+tFieldTests = makeTests_G_TH defaultOptions 
+                             [ CFieldTest1 7 8
+                             , CFieldTest2 False "a" 
+                             ]
+
+-- Test field label modifier options
 data TFieldLabelModifier = CFieldLabelModifier { fFieldLabelModifier :: Int }
-                         deriving (Show, Typeable)
+                         deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{fieldLabelModifier = drop 3} ''TFieldLabelModifier
 
+tFieldLabelModifier = makeTests_G_TH defaultOptions{fieldLabelModifier = drop 3} 
+                                     [ CFieldLabelModifier 1
+                                     , CFieldLabelModifier (-1)
+                                     ]
+
+-- Test constructor tag modifier
 data TConstructorTagModifier = CTest1 Int
                              | CTest2 Bool
-                             deriving (Show, Typeable)
+                             deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{constructorTagModifier = drop 1} ''TConstructorTagModifier
 
+tConstructorTagModifier = makeTests_G_TH defaultOptions{constructorTagModifier = drop 1}
+                                         [ CTest1 1
+                                         , CTest2 False
+                                         ]
+
+-- Test the allNullaryToStringTag flag
 data TAllNullaryToStringTag = CAllNullaryToStringTag1
                             | CAllNullaryToStringTag2
-                            deriving (Show, Typeable)
+                            deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{allNullaryToStringTag = False} ''TAllNullaryToStringTag
 
+tAllNullaryToStringTag = makeTests_G_TH defaultOptions{allNullaryToStringTag = False}
+                                        [ CAllNullaryToStringTag1 
+                                        , CAllNullaryToStringTag2
+                                        ]
+
+-- Test the allNullaryToStringTag flag
 data TAllNullaryToStringTag' = CAllNullaryToStringTag1'
                              | CAllNullaryToStringTag2'
-                             deriving (Show, Typeable)
+                             deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{allNullaryToStringTag = True} ''TAllNullaryToStringTag'
 
+tAllNullaryToStringTag' = makeTests_G_TH defaultOptions{allNullaryToStringTag = True}
+                                         [ CAllNullaryToStringTag1'
+                                         , CAllNullaryToStringTag2'
+                                         ]
+
+-- Test the `omitNothingFields` flag
 data TOmitNothingFields = COmitNothingFields (Maybe Int)
-                        deriving (Show, Typeable)
+                        deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{omitNothingFields = True} ''TOmitNothingFields
 
+tOmitNothingFields = makeTests_G_TH defaultOptions{omitNothingFields = True}
+                                    [ COmitNothingFields Nothing
+                                    , COmitNothingFields (Just 1)
+                                    ]
+
+-- Test the `sumEncoding = Tagged Object` flag
 data TTaggedObject = CTaggedObject1
                    | CTaggedObject2 Int
-                   deriving (Show, Typeable)
+                   deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{sumEncoding = TaggedObject { tagFieldName = "ttag", contentsFieldName = "ccontents" } } ''TTaggedObject
 
+tTaggedObject = makeTests_G_TH defaultOptions{sumEncoding = TaggedObject { tagFieldName = "ttag", contentsFieldName = "ccontents" } }
+                               [ CTaggedObject1
+                               , CTaggedObject2 1
+                               ]
+
+-- Test the `sumEncoding = UntaggedValue` flag
 data TUntaggedValue = CUntaggedValue1 Int
                     | CUntaggedValue2 { fUntaggedValue2 :: Text }
-                    deriving (Show, Typeable)
+                    deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{sumEncoding = UntaggedValue} ''TUntaggedValue
 
+tUntaggedValue = makeTests_G_TH defaultOptions{sumEncoding = UntaggedValue}
+                                [ CUntaggedValue1 1
+                                , CUntaggedValue2 "a"
+                                ]
+
+-- Test the `sumEncoding = ObjectWithSingleField` flag
 data TObjectWithSingleField = CObjectWithSingleField1 Int Int
                             | CObjectWithSingleField2 { fField1 :: Text, fField2 :: Text }
-                            deriving (Show, Typeable)
+                            deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{sumEncoding = ObjectWithSingleField} ''TObjectWithSingleField
 
+tObjectWithSingleField = makeTests_G_TH defaultOptions{sumEncoding = ObjectWithSingleField}
+                                        [ CObjectWithSingleField1 1 1
+                                        , CObjectWithSingleField2 "a" "a"
+                                        ]
+
+-- Test the `sumEncoding = TwoElemArray` flag
 data TTwoElemArray = CTwoElemArray1 Int
                    | CTwoElemArray2 Text
-                   deriving (Show, Typeable)
+                   deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{sumEncoding = TwoElemArray} ''TTwoElemArray
 
+tTwoElemArray = makeTests_G_TH defaultOptions{sumEncoding = TwoElemArray}
+                               [ CTwoElemArray1 1
+                               , CTwoElemArray2 "a"
+                               ]
+
+-- Test the `unwrapUnaryRecords = True` flag
 data TUnwrapUnaryRecords = CUnwrapUnaryRecords { fUnwrapUnaryRecords :: Int }
-                         deriving (Show, Typeable)
+                         deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{unwrapUnaryRecords = True} ''TUnwrapUnaryRecords
 
+tUnwrapUnaryRecords = makeTests_G_TH defaultOptions{unwrapUnaryRecords = True}
+                                     [ CUnwrapUnaryRecords 1
+                                     ]
+
+-- Test the `tagSingleConstructors = True` flag 
 data TTagSingleConstructors = CTagSingleConstructors Int
-                            deriving (Show, Typeable)
+                            deriving (Show, Typeable, Generic)
 deriveTsJSON defaultOptions{tagSingleConstructors = True} ''TTagSingleConstructors
 
-data TPolyTest a = CPolyTest a Int
-                 deriving (Show, Typeable)
+tTagSingleConstructors = makeTests_G_TH defaultOptions{tagSingleConstructors = True}
+                                        [ CTagSingleConstructors 1
+                                        ]
+
+data TPolyTest q = CPolyTest q Int
+                 deriving (Show, Typeable, Generic, Generic1)
 deriveTsJSON defaultOptions ''TPolyTest
 
-data TPolyTest2 a b = CPolyTest2 a b
-                    deriving (Show, Typeable)
+tPolyTest = makeTests_G1_TH defaultOptions $
+                            CPolyTest (1 :: Int) 7 :? CPolyTest False 7 :? TestNil
+
+data TPolyTest2 a = CPolyTest2 a (Maybe a)
+                    deriving (Show, Typeable, Generic, Generic1)
 deriveTsJSON defaultOptions ''TPolyTest2
+
+tPolyTest2 = makeTests_G1_TH defaultOptions $
+                             CPolyTest2 (1 :: Int) (Just 1) :? CPolyTest2 (1 :: Int) Nothing :? TestNil
+
+data TPolyTest3 a = CPolyTest3 (Maybe a)
+                  deriving (Show, Typeable, Generic, Generic1)
+deriveTsJSON defaultOptions ''TPolyTest3
+
+tPolyTest3 = makeTests_G1_TH defaultOptions $
+                             (CPolyTest3 Nothing :: TPolyTest3 Int) :? TestNil
 
 data TRecursiveTest a = CRecursiveBase
                       | CRecursiveRecurse a (TRecursiveTest a)
@@ -108,7 +206,7 @@ deriveTsTypeable defaultOptions ''THoleTest
 data THKTTest a = HKTTestA (a Int)
                 | HKTTestB (Maybe (a Text))
                 | HKTTestC (THoleTest (a Int))
-    deriving (Typeable)
+    deriving (Typeable, Generic)
     
 deriveTsTypeable defaultOptions ''THKTTest
 
@@ -125,42 +223,19 @@ data D' a d = D' (C' a d)
 deriveTsTypeable defaultOptions ''D'
 
 genericTests :: TestTree
-genericTests = testGroup "Aeson <-> TS generic deriving isomorphic"
-    [ makeTest (CFieldLabelModifier 1)
-    
-    , makeTest (CFieldTest1 7 8)
-    , makeTest (CFieldTest2 False "a")
-
-    , makeTest (CTest1 1)
-    , makeTest (CTest2 False)
-
-    , makeTest CAllNullaryToStringTag1
-    , makeTest CAllNullaryToStringTag2
-
-    , makeTest CAllNullaryToStringTag1'
-    , makeTest CAllNullaryToStringTag2'
-
-    , makeTest (COmitNothingFields Nothing)
-    , makeTest (COmitNothingFields (Just 1))
-
-    , makeTest CTaggedObject1
-    , makeTest (CTaggedObject2 1)
-
-    , makeTest (CUntaggedValue1 1)
-    , makeTest (CUntaggedValue2 "a")
-
-    , makeTest (CObjectWithSingleField1 1 1)
-    , makeTest (CObjectWithSingleField2 "a" "a")
-
-    , makeTest (CTwoElemArray1 1)
-    , makeTest (CTwoElemArray2 "a")
-
-    , makeTest (CUnwrapUnaryRecords 1)
-    
-    , makeTest (CTagSingleConstructors 1)
-
-    , makeTest (CPolyTest (1 :: Int) 7)
-    , makeTest (CPolyTest ("a" :: Text) 7)
-
-    , makeTest (CPolyTest2 (1 :: Int) ("a" :: Text))
-    ]
+genericTests = testGroup "Aeson <-> TS generic deriving isomorphic" $
+    tFieldTests ++
+    tFieldLabelModifier ++
+    tConstructorTagModifier ++
+    tAllNullaryToStringTag ++
+    tAllNullaryToStringTag' ++
+    tOmitNothingFields ++
+    tTaggedObject ++
+    tUntaggedValue ++
+    tObjectWithSingleField ++
+    tTwoElemArray ++
+    tUnwrapUnaryRecords ++
+    tTagSingleConstructors ++
+    tPolyTest ++
+    tPolyTest2 ++
+    tPolyTest3
--- test/Servant/TS/TestHelpers.hs
+++ test/Servant/TS/TestHelpers.hs
@@ -1,9 +1,40 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternGuards #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+#if __GLASGOW_HASKELL__ >= 800
+{-# LANGUAGE TemplateHaskellQuotes #-}
+#else
+{-# LANGUAGE TemplateHaskell #-}
+#endif
+
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+
+{-# LANGUAGE PartialTypeSignatures #-}
+
 module Servant.TS.TestHelpers (
+    tsTypecheck,
     makeTest,
-    tsTypecheck
+    makeTests_G_TH,
+    makeTests_G1_TH,
+    TestList(..)
 ) where
 
 import Data.Aeson
@@ -23,10 +54,17 @@ import qualified Data.Text as Text
 import Data.Typeable
 import Data.Vector (Vector)
 import qualified Data.Vector as Vector
+import GHC.Generics
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+import Language.Haskell.TH.Datatype
 import Test.Tasty
 import Test.Tasty.HUnit
 
 import Servant.TS
+import Servant.TS.TH
+
+import Data.Foldable (foldrM)
 
 type Context = ([Value -> Maybe (TsType, Value)], Value) -> Maybe (TsType, Value)
 
@@ -44,8 +82,8 @@ tsTypecheck t v = para f t $ ([], v)
           f (TsArrayF (_, f')) (gs, Array vs) = firstError $ f' . (gs,) <$> Vector.toList vs
           f t@(TsUnionF ts) x = if any isNothing $ ($ x) . snd <$> ts then Nothing else mkError t (snd x)
           f t@(TsObjectF ts) (gs, v@(Object m)) = if (Set.fromList . HashMap.keys) ts == (Set.fromList . HashMap.keys) m 
-                                                 then firstError $ (\((_, f), v) -> f (gs, v)) <$> (HashMap.elems $ HashMap.intersectionWith (,) ts m)
-                                                 else mkError t v
+                                                  then firstError $ (\((_, f), v) -> f (gs, v)) <$> (HashMap.elems $ HashMap.intersectionWith (,) ts m)
+                                                  else mkError t v
           f (TsMapF (_, f')) (gs, Object m) = firstError $ f' . (gs,) <$> HashMap.elems m
           f t@(TsTupleF ts) (gs, v@(Array vs)) = if length ts == length vs
                                                  then firstError $ (\((_, f), v) -> f (gs, v)) <$> zip ts (Vector.toList vs)
@@ -60,15 +98,95 @@ tsTypecheck t v = para f t $ ([], v)
           mkError :: TsTypeBaseF TsDefF (TsType, Context) -> Value -> Maybe (TsType, Value)
           mkError tF v' = Just (embed . fmap fst $ tF, v')
 
+makeTestInternal :: forall a. (Show a, Typeable a) => a -> TsType -> Value -> Assertion
+makeTestInternal v t j = case tsTypecheck t j of
+                             Nothing -> True @? ""
+                             Just (t', j') -> False @? "Failed to unify JSON value with TS type:\n" 
+                                                    ++ "    Value:  " ++ show j' ++ "\n" 
+                                                    ++ "    TsType: " ++ show t' ++ "\n" 
+                                                    ++ "Initial types:\n" 
+                                                    ++ "    Value:  " ++ show j ++ "\n" 
+                                                    ++ "    TsType: " ++ show t
+
+makeValueDesc :: forall a. (Show a, Typeable a) => a -> String
+makeValueDesc v = show v ++ " :: " ++ show (typeRep (Proxy :: Proxy a))
+
 makeTest :: forall a. (ToJSON a, Show a, Typeable a, TsTypeable a) => a -> TestTree
-makeTest v = let t = tsTypeRep (Proxy :: Proxy a)
-                 j = toJSON v
-                 a' = case tsTypecheck t j of
-                        Nothing -> True @? ""
-                        Just (t', j') -> False @? "Failed to unify JSON value with TS type:\n" 
-                                               ++ "    Value:  " ++ show j' ++ "\n" 
-                                               ++ "    TsType: " ++ show t' ++ "\n" 
-                                               ++ "Initial types:\n" 
-                                               ++ "    Value:  " ++ show j ++ "\n" 
-                                               ++ "    TsType: " ++ show t
-              in testCase (show v ++ " :: " ++ show (typeRep (Proxy :: Proxy a))) a'
+makeTest v = testCase (makeValueDesc v) $ makeTestInternal v (tsTypeRep (Proxy :: Proxy a)) (toJSON v)
+
+makeTests_G_TH :: forall a. (Show a, Typeable a, Generic a, GTsDatatype (Rep a), GToJSON Zero (Rep a), ToJSON a, TsTypeable a) => Options -> [a] -> [TestTree]
+makeTests_G_TH opts vals = [testGroup n $ singleDefTest : (testValue <$> vals)]
+    where p = Proxy :: Proxy a
+          n = show . typeRepTyCon . typeOf . head $ vals
+
+          singleDefTest = makeSingleDefinitionTest (concat . fmap makeContext $ vals)
+
+          makeContext v = [ DerivingContext (typeRep p) TH (tsTypeRep p)
+                          , DerivingContext (typeRep p) G0 (genericTsTypeable opts p) 
+                          ]
+
+          testValue v = testGroup (makeValueDesc v) $ 
+              [ testCase "G0 <-> Aeson " $ makeTestInternal v (genericTsTypeable opts p) (genericToJSON opts v)
+              , testCase "TH <-> Aeson " $ makeTestInternal v (tsTypeRep p) (toJSON v)
+              , testCase "G0 = TH" ((tsTypeRep p) @=? (genericTsTypeable opts p))
+              ]
+
+data TestList (ls :: [*]) where 
+    TestNil :: TestList '[]
+    (:?) :: forall a p xs. (Show (a p), TsTypeable (a p), TsTypeable p, Typeable (a p), Typeable a, Generic (a p), GToJSON Zero (Rep (a p)), Generic1 a, GTsDatatype1 (Rep1 a p), GTsDatatype (Rep (a p)), ToJSON (a p)) => a p -> TestList xs -> TestList ((a p) ': xs)
+
+infixr 7 :?
+
+data DerivingMethod = TH
+                    | G0
+                    | G1
+                    deriving (Show, Eq, Enum)
+
+data DerivingContext a = DerivingContext TypeRep DerivingMethod a
+
+makeSingleDefinitionTest :: [DerivingContext TsType] -> TestTree
+makeSingleDefinitionTest ts = testCase "Single Definition" $ case foldrM f Map.empty ts of
+    Left s -> False @? s 
+    Right _ -> True @? ""
+
+    where f :: DerivingContext TsType -> Map TsTypeName (DerivingContext TsDef) -> Either String (Map TsTypeName (DerivingContext TsDef))
+          f (DerivingContext tr dm t) m = case t of
+              (TsNamedType tn _ td) -> let tn' = stripGenerics tn
+                                        in case Map.lookup tn m of
+                                               Just (DerivingContext tr' dm' td') -> 
+                                                   if td == td' 
+                                                   then Right m 
+                                                   else Left $ "Found diverging definitions for type:\n"
+                                                               ++ "    Type name: " ++ show tn ++ "\n"
+                                                               ++ "    Def 1 (" ++ show dm  ++ " - " ++ show tr  ++ "): " ++ show td ++ "\n"
+                                                               ++ "    Def 2 (" ++ show dm' ++ " - " ++ show tr' ++ "): " ++ show td'
+                                               Nothing -> Right $ Map.insert tn (DerivingContext tr dm td) m 
+              _ -> Left $ "Expected TsNamedType, but got " ++ show t
+
+          stripGenerics :: TsTypeName -> TsTypeName
+          stripGenerics (TsTypeName cn ts i) = TsTypeName cn ts 0
+
+makeTests_G1_TH :: Options -> TestList xs -> [TestTree]
+makeTests_G1_TH opts xs = [testGroup (n xs) $ makeSingleDefinitionTest (unwrapTsTypes xs) : unwrapTests xs]
+
+    where n :: TestList xs -> String
+          n (v :? _) = show . typeRepTyCon . typeOf $ v
+        
+          unwrapTsTypes :: TestList xs -> [DerivingContext TsType]
+          unwrapTsTypes TestNil = []
+          unwrapTsTypes ((v :: a p) :? vs) =
+              let p = Proxy :: Proxy (a p)
+                  mkContext = DerivingContext (typeRep p)
+                  th = mkContext TH (tsTypeRep p)
+                  g0 = mkContext G0 (genericTsTypeable opts p)
+                  g1 = mkContext G1 (generic1TsTypeable opts p)
+               in [th, g0, g1] ++ unwrapTsTypes vs
+
+          unwrapTests :: TestList xs -> [TestTree]
+          unwrapTests TestNil = []
+          unwrapTests ((v :: a p) :? vs) = testGroup (show v ++ " :: " ++ show (typeRep p)) [test_TH, test_G0, test_G, test_EQ1] : unwrapTests vs
+              where p = Proxy :: Proxy (a p)
+                    test_TH = testCase "TH <-> Aeson" $ makeTestInternal v (tsTypeRep p) (toJSON v)
+                    test_G0 = testCase "G0 <-> Aeson" $ makeTestInternal v (genericTsTypeable opts p) (genericToJSON opts v)
+                    test_G = testCase "G1 <-> Aeson" $ makeTestInternal v (generic1TsTypeable opts p) (genericToJSON opts v)
+                    test_EQ1 = testCase "G1 = TH" ((tsTypeRep p) @=? (generic1TsTypeable opts p))
